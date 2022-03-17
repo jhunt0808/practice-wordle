50c1afc365726e1d54138351e70cec9d033d739b
Josh <joshuahunt82@gmail.com>
2022-03-16 22:53:10 -0500

    attempt to fix ga, style keys

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import Game from './components/Game/Game';
 import ReactGA from 'react-ga';
 export default class App extends React.Component {
   
+
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  
   render() {
 
     var host = window.location.hostname;
@@ -13,7 +19,6 @@ export default class App extends React.Component {
       console.log('not local');
       ReactGA.initialize(TRACKING_ID,
        { debug:true, standardImplementation: true });
-       ReactGA.pageview(window.location.pathname);
     }
 
     return (
